2b7807f8f7e670c9eb7d8fd410a6afcdbedca9b6
jenguo <zhaifengguo@gmail.com>
2015-03-11 17:26:47 +0800

    a

---
+++ Test/InitDev.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+flag=`fdisk -l | sed -n `/dev\/xvdb1/p'`
+
+if [[ $flag -eq "" ]];then
+   fdisk /dev/xvdb << EOF
+n
+p
+1
+t
+1
+8e
+
+
+wq
+EOF
+
+protprobe
+
+pvcreate /dev/xvdb1
+vgcreate vgdata /dev/xvdb1
+lvcreate -l 100%FREE -n lvdata vgdata
+mkfs.ext4 /dev/vgdata/lvdata > /dev/null
+
+mount /dev/vgdata/lvdata /home
+mkdir /root/backup -p
+cp /etc/fstab /root/backup/
+
+echo "/dev/vgdata/lvdata   /home    ext4    defaults     1  2" >> /etc/fstab
+
+fi
